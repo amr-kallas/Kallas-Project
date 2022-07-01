caa9b16d6e5b8023eb5d52caa55b88391d00781b
HP <amrkallas8@gmail.com>
2022-07-01 23:24:45 +0300

    f

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let other=document.querySelector(".other");
 let meagmenu=document.querySelector(".meagmenu")
 other.addEventListener("click",(eo) => {
-    meagmenu.classList.add("active")
+    meagmenu.classList.toggle("active")
 })
